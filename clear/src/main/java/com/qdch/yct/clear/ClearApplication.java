9900deb39e8d724cb02ec6e44b216b0acab68e7b
sniper <sdqdtxl@gmail.com>
2020-05-18 17:31:32 +0800

    Initial commit

---
+++ clear/src/main/java/com/qdch/yct/clear/ClearApplication.java
@@ -0,0 +1,13 @@
+package com.qdch.yct.clear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearApplication.class, args);
+    }
+
+}
